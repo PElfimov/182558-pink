c5f2b141f97319c06a1226e64b5c45ba02dea8b5
Павел <pelfimov@ya.ru>
2017-08-02 22:49:33 +0700

    gbie JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var mainnav = document.querySelector(".main-nav__toggler");
-if (mainnav) {
-  var foundhotel = document.querySelector(".page-content-index-form-btn-open-closed");
-  popup.classList.add("page-content-index-appointment-form-close");
-  foundhotel.addEventListener("click", function(event) {
+var togler = document.querySelector(".main-nav__toggler");
+var mainnav = document.querySelector(".main-nav__list");
+
+togler.addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.toggle("page-content-index-appointment-form-close");
-    console.log("Клик по ссылке вход");
+    console.log("нажата кнопка");
+
+    mainnav.classList.toggle("main-nav--close");
+    // console.log("Клик по ссылке вход");
   });
-}
